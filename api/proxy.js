a628c3e03530aa6397f82a43a68d9400df3dbee0
kirito1412221 <2052753456@qq.com>
2022-07-30 21:18:54 +0800

    依旧存在http问题

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
     // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
     if (req.url.startsWith('/api')) {
         // 这里填目标地址
-        target = 'https://gmall-h5-api.atguigu.cn'
+        target = 'http://gmall-h5-api.atguigu.cn'
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
